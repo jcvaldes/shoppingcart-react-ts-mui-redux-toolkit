c59d2e125ccb1aa2fbf1da1298f7336a444f403f
Juan Carlos Valdés <idevkingos@gmail.com>
2022-12-25 19:39:44 -0300

    config env vars

--- src/api/base.api.ts
+++ src/api/base.api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://rickandmortyapi.com/api/'
+const BASE_URL = import.meta.env.VITE_BASEURL
 
 export const instance = axios.create({
   baseURL: BASE_URL
